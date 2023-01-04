b324bc0a038d0afa6c6d7181aea774ecd5e6720c
Dhammiko Budiarto <dhammiko.budiarto@shopee.com>
2023-01-04 18:14:28 +0700

    feat(MUR-99-Product-List-Category-in-Seller-Page):tab clickable

--- src/pages/seller/[id].tsx
+++ src/pages/seller/[id].tsx
@@ -1,9 +1,11 @@
 import { useGetSellerProduct } from '@/api/product'
 import { useGetSellerInfo } from '@/api/seller'
 import { Avatar, H3 } from '@/components'
+import cx from '@/helper/cx'
 import MainLayout from '@/layout/MainLayout'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 function Seller() {
   const product = useGetSellerProduct(
@@ -18,6 +20,8 @@ function Seller() {
   const param = useRouter()
   const sellerProfile = useGetSellerInfo(param.query.id as string)
 
+  const [selectedTab, setSelectedTab] = useState('')
+
   return (
     <>
       <div>
@@ -26,7 +30,7 @@ function Seller() {
           <meta name="description" content="Murakali E-Commerce Application" />
         </Head>
         <MainLayout>
-          <div className="border-grey-200  rounded-lg border-[1px] border-solid py-5 px-8">
+          <div className="rounded-lg  border-solid  py-5 px-8">
             <div className="sm:flex ">
               <div className="flex justify-center">
                 <Avatar size="lg" url={sellerProfile.data?.data.photo_url} />
@@ -48,7 +52,7 @@ function Seller() {
                 <H3 className=" mt-5 flex gap-4">
                   Total Rating
                   <span className="text-blue-700">
-                    {sellerProfile.data?.data.total_rating} Rating{' '}
+                    {sellerProfile.data?.data.total_rating} Rating
                   </span>
                 </H3>
                 <H3 className=" mt-5 flex gap-4">
@@ -57,6 +61,44 @@ function Seller() {
                     {sellerProfile.data?.data.rating_avg}
                   </span>
                 </H3>
+              </div>
+            </div>
+          </div>
+          <div className="flex-grow ">
+            <div className="tabs tabs-boxed flex justify-between bg-base-300">
+              <button
+                onClick={() => setSelectedTab('tab-1')}
+                className={cx(
+                  ' tab-lg my-auto flex w-52 items-center justify-center text-center',
+                  selectedTab === 'tab-1' ? 'tab-active' : ''
+                )}
+              >
+                Shipping Option
+              </button>
+              <a className="tab-lg my-auto flex w-52 items-center justify-center text-center">
+                Tab 2
+              </a>
+              <a className="tab-lg my-auto flex w-52 items-center justify-center text-center">
+                Tab 3
+              </a>
+              <div className="dropdown">
+                <label
+                  tabIndex={0}
+                  className="btn-outline btn-primary  btn m-1 gap-2"
+                >
+                  Shipping Option
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu rounded-box z-50 w-52 bg-base-100 p-2 shadow"
+                >
+                  <li>
+                    <a>Item 1</a>
+                  </li>
+                  <li>
+                    <a>Item 2</a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
