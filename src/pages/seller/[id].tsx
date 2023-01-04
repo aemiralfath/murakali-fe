f64bf9e24df2eb3e030e98638a46111503e30009
Dhammiko Budiarto <dhammiko.budiarto@shopee.com>
2023-01-04 16:57:42 +0700

    feat(MUR-99-Product-List-Category-in-Seller-Page):fetch shop from BE

--- src/pages/seller/[id].tsx
+++ src/pages/seller/[id].tsx
@@ -1,10 +1,12 @@
 import { useGetSellerProduct } from '@/api/product'
+import { useGetSellerInfo } from '@/api/seller'
 import { Avatar, H3 } from '@/components'
 import MainLayout from '@/layout/MainLayout'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 function Seller() {
-  const userProfile = useGetSellerProduct(
+  const product = useGetSellerProduct(
     2,
     1,
     'rose'
@@ -13,8 +15,8 @@ function Seller() {
     // 'categoryName',
     // 'asc'
   )
-  // eslint-disable-next-line no-console
-  console.log(userProfile)
+  const param = useRouter()
+  const sellerProfile = useGetSellerInfo(param.query.id as string)
 
   return (
     <>
@@ -27,30 +29,33 @@ function Seller() {
           <div className="border-grey-200  rounded-lg border-[1px] border-solid py-5 px-8">
             <div className="sm:flex ">
               <div className="flex justify-center">
-                <Avatar
-                  size="lg"
-                  url={
-                    'https://res.cloudinary.com/dhpao1zxi/image/upload/v1672385947/afdemumjkucfm826c1xm.jpg'
-                  }
-                />
+                <Avatar size="lg" url={sellerProfile.data?.data.photo_url} />
               </div>
 
               <div className="col-span-4 mx-5 flex-grow ">
                 <H3 className=" mt-5 flex gap-4">
                   Shop Name
-                  <span className="text-blue-700">Fajar Shop</span>{' '}
+                  <span className="text-blue-700">
+                    {sellerProfile.data?.data.name}
+                  </span>
                 </H3>
                 <H3 className=" mt-5 flex gap-4">
                   Total Product
-                  <span className="text-blue-700">159 Products</span>
+                  <span className="text-blue-700">
+                    {sellerProfile.data?.data.total_product} Products
+                  </span>
                 </H3>
                 <H3 className=" mt-5 flex gap-4">
                   Total Rating
-                  <span className="text-blue-700">10k Rating </span>
+                  <span className="text-blue-700">
+                    {sellerProfile.data?.data.total_rating} Rating{' '}
+                  </span>
                 </H3>
                 <H3 className=" mt-5 flex gap-4">
                   Rating Avg
-                  <span className="text-blue-700">4,7</span>
+                  <span className="text-blue-700">
+                    {sellerProfile.data?.data.rating_avg}
+                  </span>
                 </H3>
               </div>
             </div>
