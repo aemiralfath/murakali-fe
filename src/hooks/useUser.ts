b97af9c45389edd76cd6149b68d27cf60789f949
Muhammad Fadhlan <muhammad.fadhlan@shopee.com>
2023-01-06 15:23:19 +0700

    feat(protected route): add protected route config

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,7 +1,5 @@
 import { useGetUserProfile } from '@/api/user/profile'
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { toast } from 'react-hot-toast'
 
 import type { UserDetail } from '@/types/api/user'
 
@@ -29,31 +27,4 @@ const useUser = () => {
   return { user, isLoading }
 }
 
-const useProtectedUser = () => {
-  const router = useRouter()
-
-  const [user, setUser] = useState<UserDetail | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
-
-  const userProfile = useGetUserProfile()
-  useEffect(() => {
-    setIsLoading(userProfile.isLoading)
-  }, [userProfile.isLoading])
-
-  useEffect(() => {
-    if (userProfile.isError) {
-      toast.error('Session ended, please log in again')
-      router.push('/')
-    }
-  }, [userProfile.isError])
-
-  useEffect(() => {
-    if (userProfile.isSuccess) {
-      setUser(userProfile.data.data)
-    }
-  }, [userProfile.data])
-
-  return { user, isLoading }
-}
-
-export { useUser, useProtectedUser }
+export { useUser }
