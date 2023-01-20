1f28a3ea9dad177159651097d7e776c33a54ffae
Sammy Manunggal <sammy.manunggal@shopee.com>
2023-01-20 10:37:11 +0700

    feat(create voucher): create voucher done

--- src/api/seller/voucher/index.tsx
+++ src/api/seller/voucher/index.tsx
@@ -1,6 +1,6 @@
 import { authorizedClient } from '@/api/apiClient'
 import type { APIResponse, PaginationData } from '@/types/api/response'
-import type { VoucherData } from '@/types/api/voucher'
+import type { CreateUpdateVoucher, VoucherData } from '@/types/api/voucher'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 const profileKey = 'voucher'
@@ -23,6 +23,40 @@ export const useDeleteSellerVouchers = () => {
     async (id: string) => {
       return await authorizedClient.delete<APIResponse<null>>(
         '/seller/voucher/' + id
+      )
+    },
+    {
+      onSuccess: () => {
+        void queryClient.invalidateQueries([profileKey])
+      },
+    }
+  )
+}
+
+export const useCreateVouchers = () => {
+  const queryClient = useQueryClient()
+  return useMutation(
+    async (data: CreateUpdateVoucher) => {
+      return await authorizedClient.post<APIResponse<null>>(
+        '/seller/voucher',
+        data
+      )
+    },
+    {
+      onSuccess: () => {
+        void queryClient.invalidateQueries([profileKey])
+      },
+    }
+  )
+}
+
+export const useUpdateVouchers = (id: string) => {
+  const queryClient = useQueryClient()
+  return useMutation(
+    async (data: CreateUpdateVoucher) => {
+      return await authorizedClient.put<APIResponse<null>>(
+        '/seller/voucher/' + id,
+        data
       )
     },
     {
