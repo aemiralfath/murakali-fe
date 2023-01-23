ae38f6c40c2b9aa16426fa1b9839f955c9ac73fc
Julius Beckham <julius.beckham@shopee.com>
2023-01-23 20:05:40 +0700

    feat(get detail promotion seller): new page for detail promotion seller

--- src/pages/seller-panel/promotion/manage.tsx
+++ src/pages/seller-panel/promotion/manage.tsx
@@ -758,7 +758,7 @@ const ManagePromotionSeller = () => {
               selectedProduct.map((sp, index) => {
                 return (
                   <div key={index} className="flex-col py-3">
-                    <div className="flex items-center justify-center bg-base-200">
+                    <div className="flex items-center justify-start bg-base-200">
                       <div className=" flex w-fit p-1 align-middle">
                         <div className="mt-3 w-[3rem]">
                           <input
@@ -766,7 +766,6 @@ const ManagePromotionSeller = () => {
                             className={'checkbox'}
                             checked={selectedProductId.includes(sp.product_id)}
                             onClick={() => toggleSelectedProduct(sp.product_id)}
-                            // defaultChecked={selectKey.includes(key)}
                           />
                         </div>
                         <div className="flex flex-1 gap-3">
@@ -799,43 +798,6 @@ const ManagePromotionSeller = () => {
                             </P>
                           </div>
                         </div>
-                      </div>
-
-                      <div className="flex flex-1 justify-end px-5 align-middle">
-                        <Button
-                          size="sm"
-                          buttonType="ghost"
-                          onClick={() => {
-                            setSelectedProduct(
-                              selectedProduct.filter(
-                                (sp2) => sp2.product_id !== sp.product_id
-                              )
-                            )
-                            setSelectedId(
-                              selectedId.filter((id) => id !== sp.product_id)
-                            )
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1..product_id
-                            )
-                            setSelectedProductId(tempSelect)
-                          } else {
-                            setSelectedProductId([])
-                          }5"
-                            stroke="currentColor"
-                            className="h-6 w-6"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
-                        </Button>
                       </div>
                     </div>
 
