d82132ea4a76dbf53f31b1ee9af0cc3f805ca1f7
Muhammad Fadhlan <muhammad.fadhlan@shopee.com>
2023-01-31 18:50:18 +0700

    fix(product page): cannot select 0 index

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  trailingComma: "es5",
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-};
+  importOrder: [
+    '^react(.*)',
+    'next/(.*)',
+    '@/(.*)',
+    '<THIRD_PARTY_MODULES>',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+}
